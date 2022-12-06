824dee245d5c47297f20f88a5f4e8fcd41995437
Alexsander Marçal da Silva <oalexsanderm@gmail.com>
2022-12-06 10:21:28 -0300

    Delete Javascript/Exercicios directory

--- Javascript/Exercicios/Ex2.js
+++
@@ -1,7 +0,0 @@
-// Escreva uma função que receba a idade de uma pessoa em anos e retorne a mesma idade em dias. desconsideranço anos bissextos e dias decorridos desde o último aniversário
-
-function converterIdade(anos){
-    return anos * 365
-}
-
-console.log(converterIdade(25))
