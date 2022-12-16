f8bc7df5709584f3113058706b402563708b9b4c
Alexsander Marçal da Silva <oalexsanderm@gmail.com>
2022-12-16 09:30:02 -0300

    Add files via upload

--- Javascript/objeto/heranca1.js
+++ Javascript/objeto/heranca1.js
@@ -8,5 +8,12 @@ const volvo = {
     velMax: 200
 }
 
-console.log(ferrari.__proto__);
-console.log(ferrari.__proto__ === Object.prototype);
+console.log(ferrari.__proto__)
+console.log(ferrari.__proto__ === Object.prototype)
+console.log(volvo.__proto__ === Object.prototype)
+console.log(Object.prototype.__proto__ === null)
+
+
+function MeuObjeto() {}
+console.log(typeof Object, typeof MeuObjeto)
+console.log(Object.prototype, MeuObjeto.prototype)
